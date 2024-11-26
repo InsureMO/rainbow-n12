df021522c32e3e1391ab605cca5bce682d4a0f16
brad.wu <brad.wu@ebaotech.com>
2024-11-26 18:05:13 +0800

    init

--- n12-backend/src/modules/import-data/ask-import-config-list.ts
+++ n12-backend/src/modules/import-data/ask-import-config-list.ts
@@ -1,15 +1,26 @@
-import {APIPublisher, snippet, useSnippet} from '../utils';
+import {ImportType, Tenanted} from '@rainbow-n12/shared-model';
+import {APIPublisher, asT, buildFromInput, buildSnippet, Steps} from '../utils';
 import {ImportDataRoutes} from './routes';
+import {TypeOrmBasis} from '@rainbow-o23/n3';
 
 interface AskImportConfigRequest {
-	type?: string;
+	type?: ImportType;
 	pageSize?: number;
 	pageNumber?: number;
 }
 
+interface LoadCriteria extends TypeOrmBasis {
+	sql: string;
+	params: {
+		type?: ImportType;
+		offset: number;
+		size: number;
+	};
+}
+
 export const AskImportConfigList = () => {
-	const ValidateCriteria = useSnippet('Validate criteria', {
-		snippet: snippet<AskImportConfigRequest, AskImportConfigRequest>(async ($factor, _, $) => {
+	const ValidateCriteria = Steps.snippet('Validate criteria', {
+		snippet: buildSnippet<AskImportConfigRequest, AskImportConfigRequest>(async ($factor, _, $) => {
 			const {type, pageSize, pageNumber} = $factor ?? {};
 			return {
 				type,
@@ -20,11 +31,30 @@ export const AskImportConfigList = () => {
 			};
 		})
 	});
+	const LoadConfigList = Steps.useLoadBySQL('Load import config list', {
+		fromInput: buildFromInput<AskImportConfigRequest, LoadCriteria>(async ($factor, request, $) => {
+			const {type, pageSize, pageNumber} = $factor;
+			const tenantId = asT<Tenanted>(request.$context?.authorization)?.tenantId;
+			const tenantFilter = $.touch(tenantId).isBlank().ok() ? '' : 'TENANT_ID = $tenantId';
+			const typeFilter = $.touch(type).isBlank().ok() ? '' : 'TYPE = $type';
+			const where = $.touch([tenantFilter, typeFilter].filter(Boolean).join(' AND '))
+				.isNotBlank()
+				.success((filters: string) => `WHERE ${filters}`)
+				.failure(() => '');
+
+			return {
+				sql: `SELECT CONFIG_ID AS "configId", CODE AS "code", NAME AS "name"
+                      FROM T_IMPORT_CONFIG ${where}
+                      ORDER BY CODE $.limit($offset, $size)`,
+				params: {tenantId, type, offset: (pageNumber - 1) * pageSize, size: pageSize}
+			};
+		})
+	});
 
 	return APIPublisher
 		.use(ImportDataRoutes.AskImportConfigList)
 		.authenticated()
 		.enable()
-		.steps(ValidateCriteria)
+		.steps(ValidateCriteria, LoadConfigList)
 		.publish();
 };
--- n12-backend/src/modules/utils/functions.ts
+++ n12-backend/src/modules/utils/functions.ts
@@ -1,10 +1,11 @@
 import {PipelineStepData} from '@rainbow-o23/n1';
-import {PerformFunc} from '@rainbow-o23/n3';
+import {GetInFragmentFromRequestFunc, PerformFunc, SetOutFragmentToResponseFunc} from '@rainbow-o23/n3';
 import {
 	DefaultSteps,
 	ExposedPipelineDef,
 	PipelineStepBuilderOptions,
-	SnippetPipelineStepBuilderOptions
+	SnippetPipelineStepBuilderOptions,
+	TypeOrmBySQLPipelineStepBuilderOptions
 } from '@rainbow-o23/n4';
 import {APIDefMeta, Step} from '../types';
 
@@ -15,19 +16,35 @@ export const apiDefMeta = (code: string, route: string, method: ExposedPipelineD
 	}
 	return def;
 };
+/** assert given value to be of type T */
 export const asT = <T>(value: any): T => value;
-export const func = <F extends Function>(func: F): F => func;
-export const snippet = <In extends any, Out extends any>(func: PerformFunc<PipelineStepData<In>, In, Out>): PerformFunc<PipelineStepData<In>, In, Out> => func;
-
+/** assert given function to be of type F */
+export const asF = <F extends Function>(func: F): F => func;
+/** create given step def to be of type Step */
 export const asStep = <O extends PipelineStepBuilderOptions>(def: Omit<Step<O>, 'type'>): Step<O> => {
 	const step = asT<Step<O>>(def);
 	step.type = 'step';
 	return step;
 };
 
-export const useSnippet = (name: string, def: Omit<SnippetPipelineStepBuilderOptions, 'name' | 'use' | 'step'>): Step<SnippetPipelineStepBuilderOptions> => {
+const useSnippet = (name: string, def: Omit<SnippetPipelineStepBuilderOptions, 'name' | 'use' | 'step'>): Step<SnippetPipelineStepBuilderOptions> => {
 	const step = asT<Step<SnippetPipelineStepBuilderOptions>>(def);
 	step.name = name;
 	step.use = DefaultSteps.SNIPPET;
 	return asStep(step);
 };
+const useTypeOrmLoadBySQL = (name: string, def: Omit<TypeOrmBySQLPipelineStepBuilderOptions, 'name' | 'use' | 'step' | 'sql'> & Partial<Pick<TypeOrmBySQLPipelineStepBuilderOptions, 'sql'>>): Step<TypeOrmBySQLPipelineStepBuilderOptions> => {
+	const step = asT<Step<TypeOrmBySQLPipelineStepBuilderOptions>>(def);
+	step.name = name;
+	step.use = DefaultSteps.TYPEORM_LOAD_MANY_BY_SQL;
+	return asStep(step);
+};
+
+export const Steps = {
+	snippet: useSnippet,
+	useLoadBySQL: useTypeOrmLoadBySQL
+};
+
+export const buildFromInput = <In extends any, InFragment extends any>(func: GetInFragmentFromRequestFunc<In, InFragment>): GetInFragmentFromRequestFunc<In, InFragment> => func;
+export const buildToOutput = <In extends any, Out extends any, OutFragment extends any>(func: SetOutFragmentToResponseFunc<In, Out, OutFragment>): SetOutFragmentToResponseFunc<In, Out, OutFragment> => func;
+export const buildSnippet = <In extends any, Out extends any>(func: PerformFunc<PipelineStepData<In>, In, Out>): PerformFunc<PipelineStepData<In>, In, Out> => func;
